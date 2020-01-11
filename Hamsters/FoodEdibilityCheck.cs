b55044ffc982c5d3bdecfa9ca3127efe717d0acd
Stan_Kudri <Stan_Kudri@DESKTOP-MFMFFL4>
2020-01-11 20:22:15 +0300

    Создал класс со словарем. В главной функции его нет

---
+++ Hamsters/FoodEdibilityCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamsters
+{
+    class FoodEdibilityCheck
+    {
+        private Dictionary<Hamsters, int> dictionary = new Dictionary<Hamsters, int>() ; 
+
+        public FoodEdibilityCheck() { }
+
+        public List<Hamsters> GetHamsters()
+        {
+            return dictionary.Select(s => s.Key).ToList();
+        }
+
+        public Dictionary<Hamsters,int> FirstDayOfTheHamster (Hamsters hamster)
+        {
+            dictionary[hamster] = 0;
+            return dictionary;
+        }
+
+        public bool IsAlive(Hamsters hamster)
+        {
+            if (dictionary[hamster] < 2)
+                return true;
+            else
+                return false;
+        }
+
+        public Dictionary<Hamsters,int> HamsterDidNotEat (Hamsters hamster)
+        {
+            dictionary[hamster]++;
+            return dictionary;
+        }
+
+        public bool Died(Hamsters hamster)
+        {
+            if (dictionary[hamster] == 2)
+                return true;
+            else
+                return false;
+        }                     
+    }
+}
--- Hamsters/Program.cs
+++ Hamsters/Program.cs
@@ -10,25 +10,53 @@ namespace Hamsters
      *Сделать перечисления с днями недели, типом еды и цветом.
      * Нужно написать кла хомяка(имя-вес-возраст-цвет) и метод, который принимает животное с днем недели и возвращает еду которой нужно кормить хомяка.
      * Написать метод принимающий хомяка и еду и выводит в консоль что случилось с хомяком. */
+    
     class Program
-    {        
+    {
+        public static bool EdibleFood(Food food)
+        {
+            if ((food == Food.candy) || (food == Food.meat))
+                return true;
+            else
+                return false;
+        }
+
         static void Main(string[] args)
         {
             var hamster = new Hamsters[2];
             hamster[0] = new Hamsters("Пупа", 400, 9, Color.pink);
             hamster[1] = new Hamsters("Трула", 500, 10, Color.white);
-            var dictionary = new Dictionary<Hamsters, int>();
+            /*var dictionary = new Dictionary<Hamsters, int>();*/
+            var edibilityCheck = new FoodEdibilityCheck();            
             for (int i = 0; i < hamster.Length; i++)
             {
-                dictionary[hamster[i]] = 0;
-                Console.WriteLine(hamster[i].ToString());
+                edibilityCheck.FirstDayOfTheHamster(hamster[i]);                
+                /*dictionary[hamster[i]] = 0; 
+                Console.WriteLine(hamster[i].ToString());*/
+            }
+            foreach (Hamsters kvp in edibilityCheck.GetHamsters())
+            {
+                Console.WriteLine(kvp);
             }
             foreach (DaysOfTheWeek deytimefood in Enum.GetValues(typeof(DaysOfTheWeek)))
             {
                 Console.WriteLine($"день недели :{deytimefood.WeekRendering()}");
-                for(int i=0; i < hamster.Length; i++)
+                foreach (Hamsters kvp in edibilityCheck.GetHamsters())
                 {
-                    if (dictionary[hamster[i]] < 2)
+                    if(edibilityCheck.IsAlive(kvp)==true)
+                    {
+                        var food = RandomFood.FoodDays();
+                        Console.WriteLine("Хомяку {0} дали {1}", kvp.Name, food.FoodRendering());
+                        if (EdibleFood(food) == true)
+                        {
+                            edibilityCheck.HamsterDidNotEat(kvp);
+                            if (edibilityCheck.Died(kvp) == true)
+                                Console.WriteLine($"{kvp.Name} умер");                            
+                        }
+                        else
+                            edibilityCheck.FirstDayOfTheHamster(kvp);
+                    }
+                    /*if (dictionary[hamster[i]] < 2)
                     {
                         var food = RandomFood.FoodDays();
                         Console.WriteLine("Хомяку {0} дали {1}", hamster[i].Name, food.FoodRendering());
@@ -42,6 +70,7 @@ namespace Hamsters
                             dictionary[hamster[i]] = 0;
 
                     }
+                    */
                 }
             }
             Console.ReadLine();
